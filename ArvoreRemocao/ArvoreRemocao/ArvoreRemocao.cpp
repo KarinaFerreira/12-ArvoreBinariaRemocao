80734aea15e8ae4a7dd4bce74bc4153e3f789a8c
KARINA FERREIRA <karina.gomes6@fatec.sp.gov.br>
2023-05-09 19:26:51 -0300

    agora foi

--- ArvoreRemocao/ArvoreRemocao/ArvoreRemocao.cpp
+++ ArvoreRemocao/ArvoreRemocao/ArvoreRemocao.cpp
@@ -246,6 +246,7 @@ void removerElementoArvore(NO* no, int valor) {
 		return;
 	}
 
+
 	// caso 1: sem filhos	
 	if (atual->dir == NULL && atual->esq == NULL) {
 		if (pai->dir->valor == atual->valor) {
